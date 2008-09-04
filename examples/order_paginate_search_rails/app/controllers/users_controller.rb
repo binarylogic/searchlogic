9e0479d7d9088b7fb1582d22d09aa39e8a9251bf
Ben Johnson <bjohnson@binarylogic.com>
2008-09-04 14:23:15 -0400

    Removed examples

--- examples/order_paginate_search_rails/app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def index
-      @search = User.new_search(params[:search])
-      @users, @users_count = @search.all, @search.count
-  end
-end
--- examples/order_paginate_search_rails/app/models/order.rb
+++
@@ -1,3 +0,0 @@
-class Order < ActiveRecord::Base
-  belongs_to :user
-end
