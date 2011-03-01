e53475502e9f6fb2ef893d8c2191020c80848302
Ben Johnson <bjohnson@binarylogic.com>
2011-02-28 23:13:29 -0500

    merge in polymorphic ordering

--- spec/searchlogic/active_record/association_proxy_spec.rb
+++ spec/searchlogic/active_record/association_proxy_spec.rb
@@ -6,19 +6,19 @@ describe "Searchlogic::ActiveRecord::AssociationProxy" do
     user = company.users.create(:username => "bjohnson")
     company.users.send(:username_like, "bjohnson").should == [user]
   end
-  
+
   it "should call ordering conditions" do
     company = Company.create
     user = company.users.create(:username => "bjohnson")
     company.users.send(:ascend_by_username).should == [user]
   end
-  
+
   it "should call 'or' conditions" do
     company = Company.create
     user = company.users.create(:username => "bjohnson")
     company.users.send(:username_or_some_type_id_like, "bjohnson").should == [user]
   end
-    
+
   it "should ignore belongs_to associations" do
     user = User.create(:male => true)
     cart = user.carts.create
--- spec/searchlogic/named_scopes/association_ordering_spec.rb
+++ spec/searchlogic/named_scopes/association_ordering_spec.rb
@@ -4,28 +4,31 @@ describe Searchlogic::NamedScopes::Ordering do
   it "should allow ascending" do
     Company.ascend_by_users_username.proxy_options.should == User.ascend_by_username.proxy_options.merge(:joins => :users)
   end
-  
+
   it "should allow descending" do
     Company.descend_by_users_username.proxy_options.should == User.descend_by_username.proxy_options.merge(:joins => :users)
   end
-  
+
   it "should allow deep ascending" do
     Company.ascend_by_users_orders_total.proxy_options.should == Order.ascend_by_total.proxy_options.merge(:joins => {:users => :orders})
   end
-  
+
   it "should allow deep descending" do
     Company.descend_by_users_orders_total.proxy_options.should == Order.descend_by_total.proxy_options.merge(:joins => {:users => :orders})
   end
-  
+
   it "should ascend with a belongs to" do
     User.ascend_by_company_name.proxy_options.should == Company.ascend_by_name.proxy_options.merge(:joins => :company)
   end
-  
+
   it "should work through #order" do
     Company.order('ascend_by_users_username').proxy_options.should == Company.ascend_by_users_username.proxy_options
   end
-  
+
   it "should ascend with a polymorphic belongs to" do
-    Audit.descend_by_auditable_user_type_username.proxy_options.should == User.descend_by_username.proxy_options.merge(:joins => :users)
+    Audit.descend_by_auditable_user_type_username.proxy_options.should ==
+      User.descend_by_username.proxy_options.merge(
+        :joins => "INNER JOIN \"users\" ON \"users\".id = \"audits\".auditable_id AND \"audits\".auditable_type = 'User'"
+      )
   end
 end
