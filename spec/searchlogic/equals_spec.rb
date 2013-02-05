12d0673560965dc721cbcf31c98674139a734950
James Vanneman <jvanneman@conciergelive.com>
2013-02-05 09:55:09 -0500

    removed valid condition

--- spec/searchlogic/equals_spec.rb
+++ spec/searchlogic/equals_spec.rb
@@ -15,4 +15,11 @@ describe Searchlogic::Conditions::Equals do
     xit "does not return a user when column doesn't exist" do 
     end 
   end
+
+  describe "Equals Query on multiple Columns" do 
+    james.email = "jvannem@gmail.com"
+    james.save 
+    User.name_equals("James").email_equals("jvannem@gmail.com").should_not be_empty
+
+  end
 end
