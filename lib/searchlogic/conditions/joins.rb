e1a624437669b24191e587b2df6fd02bdaa83b39
James Vanneman <jvanneman@conciergelive.com>
2013-02-07 17:10:24 -0500

    clean joins

--- lib/searchlogic/conditions/joins.rb
+++ lib/searchlogic/conditions/joins.rb
@@ -1,10 +1,7 @@
 module Searchlogic
   module Conditions
     class Joins < Condition
-
-      ##Send last class the method, value save the "where values"
-      ## Save where values of prev statement
-      ##Join next class over with last class
+      
       def scope
         recursive_joins if applicable?
       end
@@ -15,9 +12,7 @@ module Searchlogic
           if match_associated_model_in_method(method)
             continue_joins(klass_in_method, method)
           else
-            join_klass = klass_in_method.singularize.camelize.constantize
-            values = join_klass.send(method, value).where_values.first
-            klass.joins(klass_in_method.to_sym).where(values).uniq
+            calculate_result(klass_in_method, method)
           end
         end
 
@@ -25,6 +20,13 @@ module Searchlogic
           join_klass = klass.joins(klass_in_method.to_sym)
           join_klass.send(method, value)
         end
+
+        def calculate_result(klass_in_method, method)
+          join_klass = klass_in_method.singularize.camelize.constantize
+          values = join_klass.send(method, value).where_values.first
+          klass.joins(klass_in_method.to_sym).where(values).uniq
+        end
+
 
         def value
           args.first
