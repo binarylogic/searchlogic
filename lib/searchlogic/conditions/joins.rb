3c81badb05ce8a26184014f1982c0fd9122ae52e
James Vanneman <jvanneman@conciergelive.com>
2013-02-07 17:04:47 -0500

    refactored joins class

--- lib/searchlogic/conditions/joins.rb
+++ lib/searchlogic/conditions/joins.rb
@@ -6,34 +6,35 @@ module Searchlogic
       ## Save where values of prev statement
       ##Join next class over with last class
       def scope
-        if applicable?
-          klass_in_method = match_associated_model_and_method[1]
-          join_klass = klass_in_method.singularize.camelize.constantize
-          method = match_associated_model_and_method[2]
-          associated_models = join_klass.reflect_on_all_associations.map { |a| a.name }
-
-          re = /(^#{associated_models.join("|")})_(.*)/
-          association_in_method = re.match(method)
-          if association_in_method 
-            join_klass = klass.joins(klass_in_method.to_sym)
-            scope = join_klass.send(method, value)
-          else
-            values = join_klass.send(method, value).where_values.first
-            klass.joins(klass_in_method.to_sym).where(values).uniq
-          end
-
-          
-        end
+        recursive_joins if applicable?
       end
 
       private
+        def recursive_joins
+          klass_in_method, method = match_associated_model_in_method(method_name)[1, 2]
+          if match_associated_model_in_method(method)
+            continue_joins(klass_in_method, method)
+          else
+            join_klass = klass_in_method.singularize.camelize.constantize
+            values = join_klass.send(method, value).where_values.first
+            klass.joins(klass_in_method.to_sym).where(values).uniq
+          end
+        end
+
+        def continue_joins(klass_in_method, method)
+          join_klass = klass.joins(klass_in_method.to_sym)
+          join_klass.send(method, value)
+        end
+
         def value
           args.first
         end
+
         def applicable?
-          !( match_associated_model_and_method).nil?
+          !( match_associated_model_in_method(method_name)).nil?
         end
-        def match_associated_model_and_method
+
+        def match_associated_model_in_method(method_name)
             associated_models = klass.reflect_on_all_associations.map { |a| a.name }
             /(^#{associated_models.join("|")})_(.*)/.match(method_name)
         end
