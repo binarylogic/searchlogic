9958207b4a19e91ad36c98437e4914feeea7ef8b
James Vanneman <jvanneman@conciergelive.com>
2013-02-13 09:14:16 -0500

    added specs

--- spec/searchlogic/does_not_begin_with_spec.rb
+++ spec/searchlogic/does_not_begin_with_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Searchlogic::Conditions::DoesNotBeginWith do 
   before(:each) do 
     @james = User.create(:name=>"James")
-    @ben = User.create(:name=>"Ben")
-    @tren = User.create(:name=>"Tren")
+    @ben = User.create(:name=>"BenJam")
+    @tren = User.create(:name=>"TJamn")
   end
 
   it "finds users that do not begin with input" do 
     find_users = User.name_does_not_begin_with("Jam")
     user_names = find_users.map { |u| u.name }
-    user_names.should eq(["Ben", "Tren"])
+    user_names.should eq(["BenJam", "TJamn"])
   end
 end
--- spec/searchlogic/does_not_end_with_spec.rb
+++ spec/searchlogic/does_not_end_with_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 describe Searchlogic::Conditions::DoesNotEndWith do 
   before(:each) do 
     @james = User.create(:name=>"James")
+    User.create(:name => "Jamess")
     @bob = User.create(:name => "Bob")
   end
 
   it "finds users that do not end with input" do 
     find_users = User.name_does_not_end_with("mes")
     user_names = find_users.map { |u| u.name }
-    user_names.should eq(["Bob"])
+    user_names.should eq(["Jamess", "Bob"])
   end
 end
