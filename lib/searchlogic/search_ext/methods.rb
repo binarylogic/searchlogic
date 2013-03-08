65b91cf0360b9a56b729aa05cc4cf8098d41f284
James Vanneman <jvanneman@conciergelive.com>
2013-03-08 16:57:27 -0500

    added #to_params method to search

--- lib/searchlogic/search_ext/methods.rb
+++ lib/searchlogic/search_ext/methods.rb
@@ -1,7 +1,14 @@
+require 'cgi'
 module Searchlogic
   module SearchExt
     module Methods
-      
+
+      def to_params(namespace = nil)
+        conditions do |key, value|
+          value.to_query(namespace ? "#{namespace}[#{key}]" : key)
+        end.sort * '&'
+      end
+
       def delete(args)
         args = args.first if args.kind_of?(Array)
         conditions.delete(args.to_sym) 
--- spec/searchlogic/search/methods_spec.rb
+++ spec/searchlogic/search/methods_spec.rb
@@ -3,15 +3,19 @@ require 'spec_helper'
 describe Searchlogic::SearchExt::Methods do 
   context "#delete" do
     it "should delete the condition" do
+
+      search = User.search(:username_like => "James")
+      search.conditions.keys.should include(:username_like)
       search.delete("username_like")
       search.username_like.should be_nil
       search.conditions["username_like"].should be_nil
     end
   end  
+  
   context "#to_params" do 
     it "should convert the conditions to a query string" do 
       search = User.search(:username_like => "jvans1", :age_gte_or_id_eq => 20, :created_at_before => "yesterday")
-      binding.pry
+      search.to_params.should eq("age_gte_or_id_eq&20&created_at_before&2013-03-07 12:00:00 -0500&username_like&jvans1")
     end
 
   end
